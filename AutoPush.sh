32a4fa54389e6d6ce09b55325fcdbb694203db13
yoshiki495 <yoshiki-tanaka@cuebic.co.jp>
2022-06-21 17:52:37 +0900

    fix

--- AutoPush.sh
+++ AutoPush.sh
@@ -4,6 +4,6 @@ export SRC_DIR="/Users/yoshiki-tanaka/Develop/Personal/HackerRank"
 PATH=/usr/local/bin:$PATH
 
 cd ${SRC_DIR}
-git add *;
-git commit -m "fix";
-git push -u origin main;
+git add *
+git commit -m "fix"
+git push -u origin main
