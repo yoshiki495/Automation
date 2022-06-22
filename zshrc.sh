0d943f2f1fdd3a96ed589a95733cc4c76571ff4c
yoshiki495 <yoshiki-tanaka@cuebic.co.jp>
2022-06-22 18:19:02 +0900

    fix

---
+++ zshrc.sh
@@ -0,0 +1,166 @@
+export AWS_SDK_LOAD_CONFIG=1
+
+export AWS_PROFILE=sandbox
+
+# ------------------------------
+# General Settings
+# ------------------------------
+export EDITOR=vim        # エディタをvimに設定
+export LANG=ja_JP.UTF-8  # 文字コードをUTF-8に設定
+export KCODE=u           # KCODEにUTF-8を設定
+export AUTOFEATURE=true  # autotestでfeatureを動かす
+
+autoload -U compinit
+compinit
+zstyle ':completion:*:default' menu select=1
+
+bindkey -v              # キーバインドをviモードに設定
+
+setopt auto_pushd        # cd時にディレクトリスタックにpushdする
+#setopt correct           # コマンドのスペルを訂正する
+setopt prompt_subst      # プロンプト定義内で変数置換やコマンド置換を扱う
+setopt notify            # バックグラウンドジョブの状態変化を即時報告する
+#setopt equals            # =commandを`which command`と同じ処理にする
+
+### Complement ###
+autoload -U compinit; compinit # 補完機能を有効にする
+setopt auto_list               # 補完候補を一覧で表示する(d)
+setopt auto_menu               # 補完キー連打で補完候補を順に表示する(d)
+setopt list_packed             # 補完候補をできるだけ詰めて表示する
+setopt list_types              # 補完候補にファイルの種類も表示する
+bindkey "^[[Z" reverse-menu-complete  # Shift-Tabで補完候補を逆順する("\e[Z"でも動作する)
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' # 補完時に大文字小文字を区別しない
+
+### History ###
+HISTFILE=~/.zsh_history   # ヒストリを保存するファイル
+HISTSIZE=10000            # メモリに保存されるヒストリの件数
+SAVEHIST=10000            # 保存されるヒストリの件数
+setopt bang_hist          # !を使ったヒストリ展開を行う(d)
+setopt extended_history   # ヒストリに実行時間も保存する
+setopt hist_ignore_dups   # 直前と同じコマンドはヒストリに追加しない
+setopt share_history      # 他のシェルのヒストリをリアルタイムで共有する
+setopt hist_reduce_blanks # 余分なスペースを削除してヒストリに保存する
+
+# マッチしたコマンドのヒストリを表示できるようにする
+autoload history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^P" history-beginning-search-backward-end
+bindkey "^N" history-beginning-search-forward-end
+
+# すべてのヒストリを表示する
+function history-all { history -E 1 }
+
+# ------------------------------
+# Look And Feel Settings
+# ------------------------------
+### Ls Color ###
+# 色の設定
+export LSCOLORS=Exfxcxdxbxegedabagacad
+# 補完時の色の設定
+export LS_COLORS='di=01;34:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+# ZLS_COLORSとは？
+export ZLS_COLORS=$LS_COLORS
+# lsコマンド時、自動で色がつく(ls -Gのようなもの？)
+export CLICOLOR=true
+# 補完候補に色を付ける
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
+### Prompt ###
+# VCSの情報を取得するzshの便利関数 vcs_infoを使う
+autoload -Uz vcs_info
+
+# 表示フォーマットの指定
+# %b ブランチ情報
+# %a アクション名(mergeなど)
+zstyle ':vcs_info:*' formats '[%b]'
+zstyle ':vcs_info:*' actionformats '[%b|%a]'
+
+precmd () {
+  psvar=()
+    LANG=en_US.UTF-8 vcs_info
+    [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
+}
+# バージョン管理されているディレクトリにいれば表示，そうでなければ非表示
+RPROMPT="%1(v|%F{green}%1v%f|)"
+
+# プロンプトに色を付ける
+autoload -U colors; colors
+# 一般ユーザ時
+tmp_prompt="%F{cyan}[%n@%D{%m/%d %T}]%f%1(v|%F{green}%1v%f|) "
+#tmp_prompt="%{${fg[cyan]}%}%n%# %{${reset_color}%} %1(v|%F{green}%1v%f|)"
+tmp_prompt2="%{${fg[cyan]}%}%_> %{${reset_color}%}"
+tmp_rprompt="%{${fg[green]}%}[%~]%{${reset_color}%}"
+tmp_sprompt="%{${fg[yellow]}%}%r is correct? [Yes, No, Abort, Edit]:%{${reset_color}%}"
+
+# rootユーザ時(太字にし、アンダーバーをつける)
+if [ ${UID} -eq 0 ]; then
+  tmp_prompt="%B%U${tmp_prompt}%u%b"
+  tmp_prompt2="%B%U${tmp_prompt2}%u%b"
+  tmp_rprompt="%B%U${tmp_rprompt}%u%b"
+  tmp_sprompt="%B%U${tmp_sprompt}%u%b"
+fi
+
+PROMPT=$tmp_prompt    # 通常のプロンプト
+PROMPT2=$tmp_prompt2  # セカンダリのプロンプト(コマンドが2行以上の時に表示される)
+RPROMPT=$tmp_rprompt  # 右側のプロンプト
+SPROMPT=$tmp_sprompt  # スペル訂正用プロンプト
+# SSHログイン時のプロンプト
+[ -n "${REMOTEHOST}${SSH_CONNECTION}" ] &&
+  PROMPT="%{${fg[white]}%}${HOST%%.*} ${PROMPT}"
+;
+
+#Title
+# precmd() {
+#     [[ -t 1 ]] || return
+#     case $TERM in
+#         *xterm*|rxvt|(dt|k|E)term)
+#         print -Pn "\e]2;[%~]\a"
+# 	;;
+#         # screen)
+#         #      #print -Pn "\e]0;[%n@%m %~] [%l]\a"
+#         #      print -Pn "\e]0;[%n@%m %~]\a"
+#         #      ;;
+#     esac
+# }
+
+# ------------------------------
+# Other Settings
+# ------------------------------
+
+### Aliases ###
+#時刻を表示させる
+alias history='history -E'
+
+# lsしたときls -llにする
+alias ls='ls -ll'
+
+# cdコマンド実行後、lsを実行する
+function cd() {
+  builtin cd $@ && ls -ll;
+}
+
+#tools alias
+alias atom='open -a Atom'
+
+#alias
+alias gs='git status'
+alias ts='tig status'
+alias ga='git add'
+alias gl='git log'
+alias bys='cd /Users/a13232/monaco-web;grunt nodemon;'
+alias nl='npm list --depth=0 -g'
+alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+
+alias git all=
+'git add *
+git commit -m "fix
+git push -u origin main'
+
+
+# suffix alias
+setopt auto_cd
+alias -s html='google-chrome'
+alias -s {md,markdown,txt}='vim'
+
+export PATH=$HOME/.nodebrew/current/bin:$PATH
